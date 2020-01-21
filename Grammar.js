698aad370394281bef4479ad0b7e0b4dea7215f2
yimmit303 <timsteiner303@gmail.com>
2020-01-21 10:10:05 -0500

    Added Grammar files

---
+++ Grammar.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Grammar = /** @class */ (function () {
+    function Grammar() {
+    }
+    return Grammar;
+}());
+exports.Grammar = Grammar;
+//# sourceMappingURL=Grammar.js.map
