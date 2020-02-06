d727852d1cc2e4ed750a97cc4dd9b2dd17e0070a
yimmit303 <timsteiner303@gmail.com>
2020-02-06 10:14:47 -0500

    Lab 2 tests added

--- testharness.ts
+++ testharness.ts
@@ -1,40 +1,80 @@
+"use strict";
+
+//This test harness doesn't output many diagnostics when things break.
+//If you want to, you can instrument it accordingly to help you debug.
 
 declare var require:any;
+
+import {Tokenizer} from "./Tokenizer"
+import {Grammar} from "./Grammar"
 let fs = require("fs");
-import {Grammar} from "./Grammar";
+
 
 function main(){
-    
-    let data:string = fs.readFileSync("tests.txt","utf8");
-    let tests: any = JSON.parse(data);
+    let teststr : string = fs.readFileSync("tests.txt","utf8");
+    let tests = JSON.parse(teststr);
+    let lastSpec : string;
+    let G: Grammar;
+    let T: Tokenizer;
     
     for(let i=0;i<tests.length;++i){
-        
-        let name: string = tests[i]["name"];
-        let ok: any = tests[i]["ok"];
-        let grammar: string = tests[i]["grammar"];
-        
-        let accepted=false;
-        try{
-            let G = new Grammar( grammar );
-            accepted = true;
-        } catch(e){
-        }
-
-        if(ok){
-            if( !accepted ){
-                console.log("Rejected valid grammar "+name);
-                return;
-            }
+        console.log("Test "+i);
+        let spec = tests[i]["tokenSpec"];
+        let inp = tests[i]["input"];
+        let expected = tests[i]["expected"];
+        if( spec !== lastSpec ){
+            G = new Grammar(spec);
+            T = new Tokenizer(G);
+            console.log("Creating tokenizer for "+tests[i]["gname"]+"...");
+            lastSpec=spec;
         } else {
-            if( accepted ){
-                console.log("Accepted invalid grammar "+name);
-                return;
+            console.log("Reusing tokenizer...");
+        }
+        
+        console.log("Input "+tests[i]["iname"]);
+        T.setInput(inp);
+        
+        let j = 0;
+        while(true){
+            
+            let expectedToken = expected[j++];
+
+            try{
+                let tok = T.next();
+                 
+                if( expectedToken === undefined ){
+                    console.log("Did not expect to get token here");
+                    return;
+                }
+                
+                if( expectedToken.sym === "$" && tok.sym === "$" ){
+                    break;
+                }
+                
+                if( tok.sym !== expectedToken.sym || tok.lexeme !== expectedToken.lexeme || tok.line !== expectedToken.line ){
+                    console.log("Mismatch");
+                    console.log("\tGot:",tok);
+                    console.log("\tExpected:",expectedToken);
+                    console.log("\tGrammar:");
+                    console.log(""+spec);
+                    return;
+                }
+                
+            } catch(e){
+                
+                if( e ){
+                    if( expectedToken === undefined ){
+                        //failure was expected
+                        break;
+                    } else {
+                        throw(e);
+                    }
+                }
             }
+           
         }
     }
-    console.log("All tests OK");
-    return true;
+    console.log(tests.length+" tests OK");
 }
- 
-main();
+
+main()
