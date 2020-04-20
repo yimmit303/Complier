bafd69acb707b0002558d4a630cfa0a58c4ef4f9
yimmit303 <timsteiner303@gmail.com>
2020-04-19 20:29:22 -0400

    start

---
+++ runantlr.bat
@@ -0,0 +1,4 @@
+C:\Program Files\Java\jre1.8.0_144\bin\java.exe
+set ANTLR="C:\Users\Tim\Downloads\Programming_Files\Compiler\Complier\antlr-4.8-complete.jar"
+set JAVA="C:\Program Files\Java\jre1.8.0_144\bin\java.exe"
+%JAVA% -cp %ANTLR% org.antlr.v4.Tool -Dlanguage=JavaScript "%1"
--- testharness.ts
+++ testharness.ts
@@ -1,93 +1,26 @@
+"use strict";
 
 declare var require:any;
 let fs = require("fs");
-import {Grammar} from "./Grammar";
+import {parse} from "./parser";
 
 function main(){
-    let data:string = fs.readFileSync("tests.txt","utf8");
-    let tests: any = JSON.parse(data);
-    let numPassed=0;
-    let numFailed=0;
-    
-    for(let i=0;i<tests.length;++i){
-        
-        let name: string = tests[i]["name"];
-        let expected: { [key:string] : string[]} = tests[i]["follow"];
-        let input: string = tests[i]["input"];
-
-        let G = new Grammar(input);
-        
-        let first : Map<string,Set<string>> = G.getFollow();
-        if( !dictionariesAreSame( expected, first ) ){
-            console.log("Test "+name+" failed");
-            ++numFailed;
-        } 
-        else
-            ++numPassed;
+    let inp = fs.readFileSync("input1.txt","utf8");
+    let root = parse( inp );
+    fs.writeFileSync( "tree.dot", root );
+    console.log("Wrote tree.dot");
+    inp = fs.readFileSync("input3.txt","utf8");
+    root = parse( inp );
+    fs.writeFileSync( "tree3.dot", root );
+    console.log("Wrote tree3.dot");
+    try{
+        inp = fs.readFileSync("input2.txt","utf8");
+        root = parse( inp );
+        console.log("Accepted invalid input2.txt");
+    } catch(e){
+        console.log("Rejected invalid input2.txt: Good.");
     }
-    console.log(numPassed+" tests OK"+"      "+numFailed+" tests failed" );
-    return numFailed==0;
 }
 
-function dictionariesAreSame( s1: { [key:string] : string[]}, s2: Map<string,Set<string>> ){
-    let M1: Map<string,Set<string>> = toMap(s1);
-    let M2 = s2;
-    
-    let k1: string[] = [];
-    let k2: string[] = [];
-    for(let k of M1.keys() )
-        k1.push(k);
-    for(let k of M2.keys() )
-        k2.push(k);
-    k1.sort();
-    k2.sort();
-    if( !listsEqual(k1,k2) ){
-        console.log("Lists not equal:",k1,k2);
-        return false;
-    }
-    for(let k of k1){
-        if( !listsEqual( M1.get(k), M2.get(k) ) ){
-            console.log("Lists not equal2:",M1.get(k), M2.get(k)  );
-            return false;
-        }
-    }
-    return true;
-}
 
-function toMap( s: { [key:string] : string[]} ) {
-    let r : Map<string,Set<string>> = new Map();
-    for(let k in s ){
-        r.set(k,new Set());
-        s[k].forEach( (x:string) => {
-            r.get(k).add(x);
-        });
-    }
-    return r;
-}
-    
-function listsEqual(L1a: any, L2a: any )
-{
-    let L1: string[] = [];
-    let L2: string[] = [];
-    L1a.forEach( (x:string) => {
-        L1.push(x);
-    });
-    L2a.forEach( (x:string) => {
-        L2.push(x);
-    });
-    
-    L1.sort();
-    L2.sort();
-    if( L1.length !== L2.length )
-        return false;
-    for(let i=0;i<L1.length;++i){
-        if( L1[i] !== L2[i] )
-            return false;
-    }
-    return true;
-}
- 
-    
-
-
-main();
+main()
